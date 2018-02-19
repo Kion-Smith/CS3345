ac0c9067794bb1df447ac41686ac292e980d5a13
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-02-18 22:27:55 -0600

    adding shells for functions

--- AVL Tree/src/AVLTree.java
+++ AVL Tree/src/AVLTree.java
@@ -1,8 +1,33 @@
 
 public class AVLTree
 {
+	Node root;
+	
 	public AVLTree() 
 	{
 		
+	}
+	public Node insertNode(Node n)
+	{
+		return n;
+	}
+	public Node rotateLeft(Node n)
+	{
+		return n;
+	}
+
+	public Node rotateRight(Node n)
+	{
+		return n;
+	}
+	
+	public int getHeight(Node n)
+	{
+		return n.height;
+	}
+	
+	public int getBalance(Node n)
+	{
+		return 0;
 	}
 }
