c1c9095d50d0acf810ab599bf3599233ec69a7fb
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-02-18 22:21:09 -0600

    Initial commit for creating AVL trees in java

---
+++ AVL Tree/src/AVLTree.java
@@ -0,0 +1,8 @@
+
+public class AVLTree
+{
+	public AVLTree() 
+	{
+		
+	}
+}
---
+++ AVL Tree/src/Node.java
@@ -0,0 +1,17 @@
+
+public class Node 
+{
+	//some data
+	//idk what the data is yet
+	public Node left;
+	public Node right;
+	
+	public int height;
+	
+	
+	public Node()
+	{
+		
+	}
+	
+}
